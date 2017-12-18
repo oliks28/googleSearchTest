175c9eff67561f97e40d8c03935aaae380a366d0
oliks28 <ken.adewale@yahoo.com>
2017-12-18 08:52:59 +0000

    Commit

--- src/test/java/StepDefinitions/googleTestSteps.java
+++ src/test/java/StepDefinitions/googleTestSteps.java
@@ -22,80 +22,60 @@ import cucumber.api.java.en.Then;
 import helper.TestBase;
 import pages.GoogleHomePage;
 
-
 public class googleTestSteps extends TestBase {
-	
-	
+
 	@Before
-	
-	//I have created a TestBase class in the helper package and i have used class inheritance to inherit functions and webdriver instances
-	public void setup() throws IOException{
+	// I have created a TestBase class in the helper package and i have used
+	// class inheritance to inherit functions and webdriver instances
+	public void setup() throws IOException {
 		initialize();
 	}
-	
+
 	@Given("^I am on the google\\.co\\.uk search page using chrome browser$")
 	public void i_am_on_the_google_co_uk_search_page_using_chrome_browser() throws Throwable {
 		// the testsiteName could be obtained from the config.properties file
-		driver.get(CONFIG.getProperty("testsiteName"));	 
-	   
+		driver.get(CONFIG.getProperty("testsiteName"));
+
 	}
-	
+
 	@Given("^I search for the searchterm \"([^\"]*)\" in the search box field$")
 	public void i_search_for_the_searchterm_in_the_search_box_field(String searchterm) throws Throwable {
-		//I have used page object design pattern to abstract functions in this test.Page is in the src/test/java folder
+		// I have used page object design pattern to abstract functions in this
+		// test.Page is in the src/test/java folder
 		GoogleHomePage ghp = PageFactory.initElements(driver, GoogleHomePage.class);
 		ghp.enterSearchItem(searchterm);
-		
 	}
-			
-    @Then("^I verify and report that the expected site \"([^\"]*)\" is on the first page of results$")
-	public void i_verify_and_report_that_the_expected_site_is_on_the_first_page_of_results(String expectedsite) throws Throwable {
-	   
-		
-		int pageNo=0;
-		boolean isThisExpectedsite=false;
-		String Gotexpectedsite="";
-		
-		while(!isThisExpectedsite){
-			pageNo++;
-			
-			List<WebElement>res = driver.findElements(By.xpath(".//*[@id='rso']/div/div/div"));
-			for(int i=0;i<res.size();i++){
-				
-			String values=	res.get(i).findElement(By.xpath("div/div/h3/a")).getAttribute("href");
-			System.out.println(values);
-			if(values.equals(expectedsite)){
-				
-				System.out.println("It is on the first page");
-				System.out.println(String.format("Expected site is found on Page Number %d", pageNo, Gotexpectedsite));
-				break;
+
+	@Then("^I verify and report that the expected site \"([^\"]*)\" is on the first page of results$")
+	public void i_verify_and_report_that_the_expected_site_is_on_the_first_page_of_results(String expectedsite)
+			throws Throwable {
+		int pageNo = 1;
+		String Gotexpectedsite = "";
+		boolean isFound = false;
+		while (!isFound) {
+			List<WebElement> res = driver.findElements(By.xpath("//div[@class='_NId']//h3/a"));
+			for (int i = 0; i < res.size(); i++) {
+				String values = res.get(i).getText();
+				String href = res.get(i).getAttribute("href");
+				System.out.println(values);
+				if (href.contains(expectedsite)) {
+					System.out.println("It is on the first page");
+					System.out.println(
+							String.format("Expected site is found on Page Number %d", pageNo, Gotexpectedsite));
+					isFound = true;
+					break;
+				}
 			}
-			}
+			pageNo = pageNo + 1;
+			Thread.sleep(3000);
 			
-			isThisExpectedsite=true;    
-		   }
-    }
-		
-		@After
-		public void quit(){
+			driver.findElement(By.xpath("//*[@id='pnnext']/span[2]")).click();
 			
-			driver.close();
-			
-			}
-    }
-	
-	
-	
+		}
+	}
 
-
-
-		
-	
-	
-
-	    
-	
-		
-	
-
-
+	@After
+	public void quit() {
+		driver.close();
+	}
+}
--- src/test/java/pages/GoogleHomePage.java
+++ src/test/java/pages/GoogleHomePage.java
@@ -1,8 +1,5 @@
 package pages;
 
-import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,22 +7,15 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class GoogleHomePage {
-	
-WebDriver driver;
-	
-	
-	@FindBy(how=How.CSS,using="#lst-ib")
-	
+
+	WebDriver driver;
+
+	@FindBy(how = How.CSS, using = "#lst-ib")
 	public static WebElement searchfield;
-	
-	
-	
-	
-	public void enterSearchItem(String searchitem){
-		
+
+	public void enterSearchItem(String searchitem) {
 		searchfield.sendKeys(searchitem);
 		searchfield.sendKeys(Keys.ENTER);
-		
 	}
-	
- }
+
+}
